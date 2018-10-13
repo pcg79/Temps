14a5fa3616dddb96fa2739a507e96a0736195ef9
Pat George <patrick.george@freeagent.com>
2018-10-13 18:38:01 +0100

    Added a widget

---
+++ app/src/main/java/com/patgeorge/temps/Widget.java
@@ -0,0 +1,88 @@
+package com.patgeorge.temps;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Widget extends AppWidgetProvider {
+
+    static void updateAppWidget(final Context context, final AppWidgetManager appWidgetManager,
+                                final int appWidgetId) {
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=Edinburgh,uk&appid=" +
+                BuildConfig.OpenWeatherMapApiKey;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject resp = null;
+
+                        try {
+//                            mStatusView.setText(status + "Resp: " + response.substring(0,500));
+                            resp = new JSONObject(response);
+                            DecimalFormat df = new DecimalFormat("#.##");
+                            df.setRoundingMode(RoundingMode.CEILING);
+                            Double kelvin = resp.getJSONObject("main").getDouble("temp");
+                            String fahrenheit = df.format(kelvin * 9/5 - 459.67);
+                            String celcius = df.format(kelvin - 273.15);
+
+                            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+                            views.setTextViewText(R.id.wFahrenheitView, fahrenheit + "°F");
+                            views.setTextViewText(R.id.wCelsiusView, celcius + "°C");
+
+                            // Instruct the widget manager to update the widget
+                            appWidgetManager.updateAppWidget(appWidgetId, views);
+                        } catch (Exception e) {
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
